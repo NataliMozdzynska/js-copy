41e14d4be73850aba00713b4be97398f524b65ba
Natalia <nmozdzynska45@gmail.com>
2019-05-04 20:49:01 +0200

    3 trzy

--- 3_Operatory/script.js
+++ 3_Operatory/script.js
@@ -1,1 +1,35 @@
+var logiczna = true;
+var logi = false ;
+console.log(logiczna == logi) ;
+//zad 1 true nie jest rowne false//
+var pierwsza = 2;
+var druga = 3;
+var resultmodulo = 0 ;
+//zad 2 po wpisaniu w konsoli nazwy zmiennej resultmodulo pojawia sie w wyniku wartosc przypisana do zmiennej czyli 0 //
 
+var one = 1;
+var two = 2;
+var joinedStrings = "" ;
+joinedStrings = one + two ;
+//zad 3 wyswietla wynik dodawania zminnych//
+
+var myNumber = 4 ;
+var myString = "4";
+console.log(myNumber==myString);
+console.log(myNumber===myString);
+//zad 4 typ przechowanej wartosci jest inny niz sama wartosc , jedna jest liczbowa inna zas tekstowa, z tad wynik w wyswietleniu raz true a w kolejnym przypadku false//
+
+var counter = 145 ;
+counter = counter+1 ;
+//zad 5 inkr wynik 146//
+
+var counter = 145 ;
+counter = counter-1 ;
+//zad 5 dekr wynik 145//
+
+var jakas = 2;
+var takas = 3 ; 
+var result = null ; 
+
+result = (jakas < takas);
+//zad 6 po wpisaniu zmiennej result wynik wyswietla true ,2 jest mniejsze niz 3 //
