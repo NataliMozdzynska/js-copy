4b1cbc220c2af23a7de65395b54f7dc4df3527d5
Natalia <nmozdzynska45@gmail.com>
2019-05-04 19:32:59 +0200

    pierwsze

--- 1_Pierwszy_skrypt/script.js
+++ 1_Pierwszy_skrypt/script.js
@@ -1,1 +1,3 @@
-
+console.log("Natalia Mozdzynska");
+//Następnie napisz w komentarzu w pliku script.js co wypisała konsola pod każdą instrukcją.
+//Hello world wyswietla sie w tym przypadku kiedy jest calkowicie ujety w ""//
