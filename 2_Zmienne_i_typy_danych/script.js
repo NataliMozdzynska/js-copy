aa1015b2eb17d283a0ee9f59c31928125e6872c0
Natalia <nmozdzynska45@gmail.com>
2019-05-04 19:55:24 +0200

    drrrugie

--- 2_Zmienne_i_typy_danych/script.js
+++ 2_Zmienne_i_typy_danych/script.js
@@ -1,1 +1,39 @@
+var licza = 6 ;
+//zmienna liczbowa przyechowuje wartosc 6//
+var string = "tekst";
+//zmienna  tekstowa przechowuje slowo tekst//
+var suma= licza + string ;
+//zmienna tekstowa pokazujaca tekst w wyniku polaczenia liczby oraz tekstu//
+var pr = true
+//zmienna typu logiczna ( bolean) pokazujaca wartosc prawda z jez ang "true"
+var spec = null
+// wartosc specjalna pokazujaca brak wartosci okreslamy jako zmienna specjalna//
+var spec2 = undefined
+// wartosc specjalna pokazuja niezdefiniowana wartosc//
 
+console.log(licza);
+console.log(typeof licza);
+
+console.log(string);
+console.log(typeof string);
+
+console.log(suma);
+console.log(typeof suma);
+
+console.log(pr);
+console.log(typeof pr);
+
+console.log(spec);
+console.log(typeof spec);
+
+
+var jeden = 3 ;
+var druga = 5 ;
+var result = 0;
+result=jeden + druga ;
+console.log(result);
+
+
+var nieprzypisuj;
+console.log(nieprzypisuj);
+//zmienna po wyswietleniu w konsoli przegladarki wystepuje jako undefined//
